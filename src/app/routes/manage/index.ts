73fb3f2a3c73606d6f7572350bc5ce8217ef6e4b
huangxiaomin <1219549841@qq.com>
2018-08-09 19:56:57 +0800

    231321

--- src/app/routes/manage/index.ts
+++ src/app/routes/manage/index.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared';
-import { NodeInputfileComponent } from './node-inputfile';
-import { NodeParamsetComponent } from './node-paramset';
 
 @NgModule({
     imports: [
         SharedModule,
     ],
-    declarations: [
-        NodeInputfileComponent,
-        NodeParamsetComponent,
-    ],
-    entryComponents: [NodeInputfileComponent, NodeParamsetComponent,]
 })
 export class ManageModule { }
